17a964af3f2000fd9213f918a26adba8e871ca35
yesspuspa2660 <61943093+yesspuspa2660@users.noreply.github.com>
2020-06-13 00:29:53 +0700

    Add files via upload
    Abstraction OOP 7

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Abstraction.Abstraction_Class;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hewan hewan;  // objek polymorphise
+
+            hewan = new Kura_kura();
+            hewan.Bergerak();
+
+            Console.WriteLine();
+            hewan = new Ular();
+            hewan.Bergerak();
+
+            Console.ReadKey();
+        }
+    }
+}
